bb7c4652c9badc99730f4ca3b2b200961c8a54b0
dinhnh <hdinhng92@gmail.com>
2021-03-05 13:17:27 +0700

    edit store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import store from './app/store';
+import store from './redux/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import App from './features/App/App';
